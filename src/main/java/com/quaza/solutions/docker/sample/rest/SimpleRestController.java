122eaae8594a374a05da7aa70fea54ef7a2a9ddb
manyce400 <fallon12>
2018-12-16 20:54:31 -0500

    Version 1.0 functionality: Verified enrolling to new tutorial grade.  All Proficiency Rankings succesfully saved.

---
+++ src/main/java/com/quaza/solutions/docker/sample/rest/SimpleRestController.java
@@ -0,0 +1,23 @@
+package com.quaza.solutions.docker.sample.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class SimpleRestController {
+
+
+    @Value("${customer.message}")
+    private String customerMessage;
+
+    //@GetMapping("/message")
+    @RequestMapping("/message")
+    private String getCustomerGreeting() {
+        System.out.println("customerMessage = " + customerMessage);
+        return customerMessage;
+    }
+
+}
